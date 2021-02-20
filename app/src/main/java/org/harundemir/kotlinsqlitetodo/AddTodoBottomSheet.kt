5191663ffada74e715ca7dfd2f030ac79da142cb
harundemir918 <bhd918@localhost.localdomain>
2021-02-20 19:25:23 +0300

    time picker added

--- app/src/main/java/org/harundemir/kotlinsqlitetodo/AddTodoBottomSheet.kt
+++ app/src/main/java/org/harundemir/kotlinsqlitetodo/AddTodoBottomSheet.kt
@@ -1,10 +1,14 @@
 package org.harundemir.kotlinsqlitetodo
 
+import android.app.TimePickerDialog
+import android.icu.text.SimpleDateFormat
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import kotlinx.android.synthetic.main.fragment_add_todo_bottom_sheet.*
+import java.util.*
 
 class AddTodoBottomSheet : BottomSheetDialogFragment() {
     companion object {
@@ -20,5 +24,26 @@ class AddTodoBottomSheet : BottomSheetDialogFragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        hourPickerButton.setOnClickListener(View.OnClickListener {
+            val calendar = Calendar.getInstance()
+            val timeSetListener = TimePickerDialog.OnTimeSetListener { _, hour, minute ->
+                calendar.set(Calendar.HOUR_OF_DAY, hour)
+                calendar.set(Calendar.MINUTE, minute)
+                hourEditText.setText(
+                    SimpleDateFormat(
+                        "HH:mm",
+                        Locale.getDefault()
+                    ).format(calendar.time)
+                )
+            }
+            TimePickerDialog(
+                context,
+                timeSetListener,
+                calendar.get(Calendar.HOUR_OF_DAY),
+                calendar.get(Calendar.MINUTE),
+                true
+            ).show()
+        })
     }
 }
